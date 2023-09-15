e957ae15905c68e5e44cbd50bdacfd134f143a9d
Archish Thakkar <archish@browserstack.com>
2023-09-15 16:28:15 +0530

    :art: added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'users/profile'
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   get 'pages/about', to: 'pages#about'
   get '/u/:id', to: 'users#profile', as: 'user'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
